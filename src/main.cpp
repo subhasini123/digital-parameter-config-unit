a57fdfbe0598117f714cf66f4063dc3a75818a63
subhasini123 <subhasini.kumari_cs.aiml24@gla.ac.in>
2026-02-11 14:00:41 +0530

    add clear display function

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,19 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_ADDRESS);
 void setup() {
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC,12)) {
+        Serial.println("OLED not found");
+        while (true);
+    }
+    display.clearDisplay();
 // write your initialization code here
 }
 
