86d870447ee57f248b3c58b737e89a36cdc944d9
subhasini123 <subhasini.kumari_cs.aiml24@gla.ac.in>
2026-02-11 14:08:20 +0530

    code reformat

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,24 @@
 #define OLED_ADDRESS 0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_ADDRESS);
+
 void setup() {
     Serial.begin(9600);
-    if (!display.begin(SSD1306_SWITCHCAPVCC,12)) {
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 12)) {
         Serial.println("OLED not found");
         while (true);
     }
     display.clearDisplay();
-// write your initialization code here
+    display.setTextColor(SSD1306_WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.println("Arduino Uno R4");
+    display.println("OLED with I2C");
+    display.println("Hello Students");
+    display.display();
+    // write your initialization code here
 }
 
 void loop() {
-// write your code here
+    // write your code here
 }
